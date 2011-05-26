1a08c87fd961a92dd03256192977e67e863d2ed0
Mike Matranga <mike@dmgctrl.com>
2011-05-26 11:53:41 -0400

    getting kph and mph now

--- calc.js
+++ calc.js
@@ -8,14 +8,24 @@ function replaceNodeText(id, newText) {
 function getGears() {
     var front = document.getElementById("front").value;
     var back = document.getElementById("back").value;
-    var combined = front/back
-    replaceNodeText("level", combined)
+    var combined = front/back;
+    replaceNodeText("level", combined.toFixed(2));
 
 // wheel circumference is based on 700c w/ 25mm tyres. a diameter of 622mm + 25cm (2111.15mm/2.11m)    
-    const wheel_circ = 2.11115
-    var meters = wheel_circ*combined
-    replaceNodeText("meters", meters)
+    const wheel_circ = 2.11115;
+    var meters = wheel_circ*combined;
+    replaceNodeText("meters", meters.toFixed(2));
+    
+    var kph60 = meters*3.6;
+    replaceNodeText("kph60", kph60.toFixed(2));
+    var mph60 = kph60/1.6;
+    replaceNodeText("mph60", mph60.toFixed(2));
 }
 
+/* 
+1 revolution == 10 meters
+100 revolutions == 1 Km
 
-    
+rev per min 3.6 
+
+*/
